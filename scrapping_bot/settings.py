c7d70d2fcbefbb1b79fd733db9a3bb6c5fe2c415
Darlene Wendy <darlenewendie@gmail.com>
2025-02-27 22:16:20 +0300

    Initial Commit

--- scrapping_bot/settings.py
+++ scrapping_bot/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'scrapping_bot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'scrapedb',
+        'USER': 'django_user',
+        'PASSWORD': '1234567',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
