eb83b86ac9ebcc303c7689db5787ee8c27a90846
Darlene Wendy <darlenewendie@gmail.com>
2025-03-02 17:18:45 +0300

    Initial Commit

--- scrapping/admin.py
+++ scrapping/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html, mark_safe
+from django.contrib.postgres.fields import JSONField
+from django.http import HttpResponse
+from django.forms import PasswordInput
+import json
+import csv
+from datetime import datetime
 
-# Register your models here.
+from .models import ScrapedData, ScrapingSchedule, ScrapingProxy
+
+# Custom widget for JSONField for better readability
+class JSONFieldPrettyWidget(admin.widgets.AdminTextareaWidget):
+    def format_value(self, value):
+        if value and isinstance(value,(dict, list)):
+            return json.dumps(value, indent=2, sort_keys=True)
+        return super().format_value(value)
