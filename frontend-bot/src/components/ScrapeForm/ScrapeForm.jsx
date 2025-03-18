cb09833fbeaa3682acd43a4567aae63b282fcea8
Darlene Wendy <darlenewendie@gmail.com>
2025-03-18 23:47:19 +0300

    Initial commit

--- frontend-bot/src/components/ScrapeForm/ScrapeForm.jsx
+++ frontend-bot/src/components/ScrapeForm/ScrapeForm.jsx
@@ -3,12 +3,22 @@ import {scrapeData} from '../../services/api';
 import './ScrapeForm.css';
 
 
-const ScrapeForm = (
-    {
-        onScrapingComplete}) =>{
-            const [formData, setFormData] = useState({
+const ScrapeForm = ({onScrapingComplete}) =>{
+    const [formData, setFormData] = useState({
                 url: '',
                 keywords: '',
                 dataType: '',
-            })}
+     })}
+
+    const [loading, setloading] = useState(false);
+    const [error, setError] = useState('');
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    }
+
             
